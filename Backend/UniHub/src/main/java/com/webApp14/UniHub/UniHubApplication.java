a58b3a73a4ef2c856c674c592e9b0e4f83a318da
Iván Sánchez <ivansanchezlabrador22@gmail.com>
2023-02-27 18:23:50 +0100

    Creación estructura del proyecto Spring boot

---
+++ Backend/UniHub/src/main/java/com/webApp14/UniHub/UniHubApplication.java
@@ -0,0 +1,13 @@
+package com.webApp14.UniHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniHubApplication.class, args);
+	}
+
+}
