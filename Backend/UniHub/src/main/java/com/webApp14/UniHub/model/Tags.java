2537c2d9bf370d7c5f8a58f878b72f22a51c3039
AthrxL <63263060+XalbertoXX@users.noreply.github.com>
2023-02-27 18:56:06 +0100

    Update Tags.java
    Creacion de tabla con compenentes necesarios para mostrar una serie de Tags por pantalla pertenecientes a cada curso

--- Backend/UniHub/src/main/java/com/webApp14/UniHub/model/Tags.java
+++ Backend/UniHub/src/main/java/com/webApp14/UniHub/model/Tags.java
@@ -1,4 +1,67 @@
 package com.webApp14.UniHub.model;
 
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class Tags {
+
+    // Attributes
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String tagName;
+
+    @Column(nullable = false, unique = true)
+    private String tagType;
+
+    @ManyToMany
+    private List<Pack> packs;
+
+    // Constructors
+    protected Tags() {
+    }
+    public Tags(String tagName, String tagType, List<Pack> packs) {
+        this.tagName = tagName;
+        this.tagType = tagType;
+        this.packs = packs;
+    }
+
+    // Setters and Getters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTagName() {
+        return tagName;
+    }
+
+    public void setTagName(String tagName) {
+        this.tagName = tagName;
+    }
+
+    public List<Pack> getPacks() {
+        return packs;
+    }
+
+    public void setPacks(List<Pack> packs) {
+        this.packs = packs;
+    }
+
+    public String getTagType() {
+        return tagType;
+    }
+
+    public void setTagType(String tagType) {
+        this.tagType = tagType;
+    }
+
+
 }
