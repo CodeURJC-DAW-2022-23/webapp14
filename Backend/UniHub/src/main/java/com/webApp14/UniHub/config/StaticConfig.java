0fbb476df7c8932e328a9b724cdd63802d702a39
Iván Sánchez <ivansanchezlabrador22@gmail.com>
2023-02-27 18:46:57 +0100

    Desarrollo controlador web para los Forms

--- Backend/UniHub/src/main/java/com/webApp14/UniHub/config/StaticConfig.java
+++ Backend/UniHub/src/main/java/com/webApp14/UniHub/config/StaticConfig.java
@@ -11,6 +11,6 @@ public class StaticConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
-        
+
     }
 }
