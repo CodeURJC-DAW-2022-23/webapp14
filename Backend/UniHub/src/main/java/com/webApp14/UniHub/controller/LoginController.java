ca6c7fc4de6a823de115ed522d5e248ebd6a8dde
Iván Sánchez <ivansanchezlabrador22@gmail.com>
2023-02-28 19:49:17 +0100

    Creando el paquete jwt
    Empezando el desarrollo de las clases necesarias que van a ir en este paquete

--- Backend/UniHub/src/main/java/com/webApp14/UniHub/controller/LoginController.java
+++ Backend/UniHub/src/main/java/com/webApp14/UniHub/controller/LoginController.java
@@ -1,13 +1,22 @@
 package com.webApp14.UniHub.controller;
 
+import com.webApp14.UniHub.security.RepositoryUserDetailsService;
+import com.webApp14.UniHub.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
 
-    @GetMapping("/LogIn")
+
+
+    @Autowired
+    private RepositoryUserDetailsService userService;
+
+    @RequestMapping("/LogIn")
     public String getLogin(Model model){
         return "login";
     }
