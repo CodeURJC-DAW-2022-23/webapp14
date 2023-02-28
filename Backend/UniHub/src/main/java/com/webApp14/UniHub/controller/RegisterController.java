9475af865eabfcaba86215e4b34c99ff919f3735
Iván Sánchez <ivansanchezlabrador22@gmail.com>
2023-02-28 17:44:58 +0100

    Cambios en varios estilos y estructuras

--- Backend/UniHub/src/main/java/com/webApp14/UniHub/controller/RegisterController.java
+++ Backend/UniHub/src/main/java/com/webApp14/UniHub/controller/RegisterController.java
@@ -1,4 +1,13 @@
 package com.webApp14.UniHub.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class RegisterController {
+    @GetMapping("/SignUp")
+    public String getRegister(Model model){
+        return "register";
+    }
 }
