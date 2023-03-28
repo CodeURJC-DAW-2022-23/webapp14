5031d4df257eb2256e605ee3119b30d82da7acb1
AthrxL <63263060+XalbertoXX@users.noreply.github.com>
2023-03-28 10:55:58 +0200

    Changes

--- Backend/UniHub/docker.ps1
+++
@@ -1,3 +0,0 @@
-docker build -t ivansanchez22/unihub:v1.0 .
-docker push ivansanchez22/unihub:v1.0 
-docker compose up 
---
+++ docker/docker.ps1
@@ -0,0 +1,2 @@
+docker build -t ivansanchez22/unihub:v1.0 -f docker/Dockerfile .
+docker push ivansanchez22/unihub:v1.0
