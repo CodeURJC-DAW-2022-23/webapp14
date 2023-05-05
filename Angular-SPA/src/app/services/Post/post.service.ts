a647f242298d60bec6dae7f045fee65b7fd5102e
AthrxL <63263060+XalbertoXX@users.noreply.github.com>
2023-05-05 18:17:00 +0200

    Actualización de pack-info y post.service

--- Angular-SPA/src/app/services/Post/post.service.ts
+++ Angular-SPA/src/app/services/Post/post.service.ts
@@ -1,36 +1,33 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Post } from 'src/app/Model/post.model';
+import {Injectable} from '@angular/core'
+import {HttpClient, HttpHeaders} from '@angular/common/http'
+import {Observable} from 'rxjs'
+import {Post} from 'src/app/Model/post.model'
 
 @Injectable({
-  providedIn: 'root'
+   providedIn: 'root',
 })
 export class PostService {
-    
-      constructor(private http: HttpClient) { }
-    
-     upvoteForm(id: number, form: any) {
-      console.log(form);
-         this.http.put(`/api/forms/${id}`, form).subscribe(
-          (response) => console.log("Form actualizado"),
-          (error) => alert("Error al registrar el post")
-         );
-    }
+   constructor(private http: HttpClient) {}
 
-      upvotePost(idForm: number, idPost:number, post: any ) {
-        return this.http.put(`/api/posts/${idForm}/comment/${idPost}`, post).subscribe();
-    }
+   upvoteForm(id: number, form: any) {
+      console.log(form)
+      this.http.put(`/api/forms/${id}`, form).subscribe(
+         (response) => console.log('Form actualizado'),
+         (error) => alert('Ha ocurrido algún problema vuelva a la anterior pantalla y después intente votar otra vez')
+      )
+   }
 
-    makeComment(formId: number, post: any) {
+   upvotePost(idForm: number, idPost: number, post: any) {
+      return this.http.put(`/api/posts/${idForm}/comment/${idPost}`, post).subscribe()
+   }
+
+   makeComment(formId: number, post: any) {
       console.log(formId)
       console.log(post)
-    const url = `/api/posts/comment/${formId}`; 
-    return this.http.post(url, post).subscribe(
-          (response) => console.log("Form actualizado"),
-          (error) => alert("Error al registrar el post")
-    );
-  }
-
-
- }
+      const url = `/api/posts/comment/${formId}`
+      return this.http.post(url, post).subscribe(
+         (response) => console.log('Form actualizado'),
+         (error) => alert('Error al registrar el post')
+      )
+   }
+}
