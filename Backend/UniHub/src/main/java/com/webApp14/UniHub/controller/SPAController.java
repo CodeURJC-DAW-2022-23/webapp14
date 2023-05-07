2c37e1294b6e6635d5901cd813534f2076112339
Iván Sánchez <ivansanchezlabrador22@gmail.com>
2023-05-07 17:59:34 +0200

    avances docker

---
+++ Backend/UniHub/src/main/java/com/webApp14/UniHub/controller/SPAController.java
@@ -0,0 +1,13 @@
+package com.webApp14.UniHub.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SPAController {
+
+    @GetMapping({"/new/**/{path:[^\\.]*}", "/{path:new[^\\.]*}"})
+    public String redirect() {
+        return "forward:/new/index.html";
+    }
+}
